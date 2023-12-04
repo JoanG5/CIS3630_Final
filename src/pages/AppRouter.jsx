137522f9085f536e72d58cf5bff57fa7c8d89fc0
JoanG5 <Joanguzman553@gmail.com>
2023-12-03 19:46:08 -0500

    added signup and login to the navbar started login and signup

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -4,13 +4,19 @@ import NewGames from "./NewGames";
 import OldGames from "./OldGames";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Signup from "../components/Signup";
+import Login from "../components/Login";
 
 function AppRouter() {
   return (
     <>
-      <Navbar/>
+      <Navbar
+        login={false}
+      />
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/newgames" element={<NewGames/>}/> 
         <Route path="/oldgames" element={<OldGames/>}/>
         <Route path="/cart" element={"cart component"}/> 
